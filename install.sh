19f21e8d7719e7ce21c32d7f8b76955c06d28691
wornbb <yishen.dev@gmail.com>
2023-06-12 01:10:48 -0400

    restructure

--- install.sh
+++ install.sh
@@ -8,20 +8,17 @@ SetupHome=$PWD
 PATH=$PWD/env/bin:$PATH
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    url=https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-x86_64.sh
-    DL="curl"
-    flag="-o"
+    url=https://repo.anaconda.com/archive/Anaconda3-2021.11-MacOSX-x86_64.sh
 else
-    url=https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
-    DL="wget"
-    flag="-O"
+    url=https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
 fi
 
 script=anaconda.sh
 if [[ ! -e $SetupHome/env/bin/conda ]]; then
-    $DL $url $flag $script
+    wget $url -O $script
     /bin/bash $script -u -p $SetupHome/env -b
     rm -rf $script
-    conda install -yc conda-forge/label/cf202003 ansible ansible-lint
+    conda install -yc anaconda jinja2
+    conda install -yc conda-forge/label/main ansible ansible-lint
 fi
 $SetupHome/env/bin/ansible-playbook $SetupHome/ansible/install_all.yml
