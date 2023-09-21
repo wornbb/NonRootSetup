2d768db79dc7af32f7094af9cc657ec60ea72381
Yi Shen <yishe2@cisco.com>
2023-09-21 09:23:52 -0700

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
+
 if [[ ! -e install.sh ]]; then
     echo >&2 "Please cd into the repository before running this script."
     exit 1
@@ -7,21 +9,11 @@ fi
 SetupHome=$PWD
 PATH=$PWD/env/bin:$PATH
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    url=https://repo.anaconda.com/archive/Anaconda3-2023.03-1-MacOSX-x86_64.sh
-else
-    url=https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
-fi
+PYTHON_HOME="$SetupHome"/env/yienv3
+python3 -m venv --system-site-packages "$PYTHON_HOME"
+source "$PYTHON_HOME"/bin/activate
+ansible-playbook "$SetupHome"/installers/setup_configs.yml
 
-script=anaconda.sh
-if [[ ! -e $SetupHome/env/bin/conda ]]; then
-    wget $url -O $script
-    /bin/bash $script -u -p $SetupHome/env -b
-    rm -rf $script
-    conda install -yc anaconda jinja2
-    conda install -yc conda-forge/label/main ansible ansible-lint
-fi
-$SetupHome/env/bin/ansible-playbook $SetupHome/installers/setup_configs.yml
 
 source ~/.zprofile
 python3 ./installers/install_util.py
